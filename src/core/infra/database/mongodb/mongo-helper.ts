9e649a6a29a827266e43911460f6804624c66d7d
Rafael Papastamatiou <rafael@papastamatiou.com>
2022-02-17 16:52:03 -0300

    remove MongoHelper interface

--- src/core/infra/database/mongodb/mongo-helper.ts
+++ src/core/infra/database/mongodb/mongo-helper.ts
@@ -2,12 +2,7 @@ import { Document } from 'mongoose';
 
 import { Entity } from '@core/domain/entities/entity';
 
-interface IMongoHelper {
-  mapToClass: <T extends Entity>(doc: Document) => T;
-  mapToDocument: <T extends Document>(obj: Entity) => T;
-}
-
-export const MongoHelper: IMongoHelper = {
+export const MongoHelper = {
   mapToClass: <T extends Entity>(doc: Document<T>): T => {
     const { _id, ...rest } = doc.toObject();
 
