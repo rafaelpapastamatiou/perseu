f0d1452d1f648dbafc155d4ea374513dd1c934d2
Rafael Papastamatiou <rafael@papastamatiou.com>
2022-03-02 16:52:41 -0300

    fix(bcryptadapter): fix bcrypt import

--- src/infra/providers/crypto/bcrypt.adapter.ts
+++ src/infra/providers/crypto/bcrypt.adapter.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 
 import { Comparer } from '@application/providers/crypto/comparer';
 import { Hasher } from '@application/providers/crypto/hasher';
@@ -6,11 +6,11 @@ import { Hasher } from '@application/providers/crypto/hasher';
 export class BcryptAdapter implements Hasher, Comparer {
   constructor(private readonly salt: number) {}
 
-  async hash(plaitext: string): Promise<string> {
-    return bcrypt.hash(plaitext, this.salt);
+  async hash(plaintext: string): Promise<string> {
+    return bcrypt.hash(plaintext, this.salt);
   }
 
-  async compare(plaitext: string, digest: string): Promise<boolean> {
-    return bcrypt.compare(plaitext, digest);
+  async compare(plaintext: string, digest: string): Promise<boolean> {
+    return bcrypt.compare(plaintext, digest);
   }
 }
