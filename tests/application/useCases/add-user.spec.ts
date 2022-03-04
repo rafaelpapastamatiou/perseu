44349a37626aaa5d2b91ef73a4c174f0994cdf8f
Rafael Papastamatiou <rafael@papastamatiou.com>
2022-03-04 13:02:56 -0300

    test(adduserspec): add generatedId and User.create tests

--- tests/application/useCases/add-user.spec.ts
+++ tests/application/useCases/add-user.spec.ts
@@ -8,6 +8,7 @@ import {
 import { UsersRepositoryStub } from '@tests/infra/mocks/repositories/users.repository.stub';
 import { HasherStub } from '@tests/infra/mocks/providers/hasher.stub';
 import { UserSerializerStub } from '@tests/infra/mocks/providers/serializers/user.serializer.stub';
+import { User } from '@domain/entities/user';
 
 const makeSut = () => {
   const usersRepositoryStub = new UsersRepositoryStub();
@@ -43,7 +44,7 @@ describe('AddUser', () => {
       .spyOn(usersRepositoryStub, 'findByEmail')
       .mockResolvedValueOnce(mockedUser);
 
-    expect(sut.execute(mockedUserData)).rejects.toThrow(
+    await expect(sut.execute(mockedUserData)).rejects.toThrow(
       EmailAlreadyInUseException,
     );
   });
@@ -55,6 +56,26 @@ describe('AddUser', () => {
       throw new Error();
     });
 
-    expect(sut.execute(mockedUserData)).rejects.toThrow();
+    await expect(sut.execute(mockedUserData)).rejects.toThrow();
+  });
+
+  it('should throw if generateId fails', async () => {
+    const { sut, usersRepositoryStub } = makeSut();
+
+    jest
+      .spyOn(usersRepositoryStub, 'generateId')
+      .mockResolvedValueOnce(undefined);
+
+    await expect(sut.execute(mockedUserData)).rejects.toThrow();
+  });
+
+  it('should throw if user creation fails', async () => {
+    const { sut } = makeSut();
+
+    jest.spyOn(User, 'create').mockReturnValueOnce(undefined);
+
+    const promise = sut.execute(mockedUserData);
+
+    await expect(promise).rejects.toThrow();
   });
 });
