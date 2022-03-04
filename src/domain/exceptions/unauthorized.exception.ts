d55a41eaef4926643de6a9404a33ea73b9b830cb
Rafael Papastamatiou <rafael@papastamatiou.com>
2022-03-04 13:01:00 -0300

    refactor(unauthorizedexception): remove default message

--- src/domain/exceptions/unauthorized.exception.ts
+++ src/domain/exceptions/unauthorized.exception.ts
@@ -2,7 +2,7 @@ import { DomainExceptionCodes } from '../enums/domain-exception-codes.enum';
 import { DomainException } from './domain-exception';
 
 export class UnauthorizedException extends DomainException {
-  constructor(message = 'Unauthorized') {
+  constructor(message: string) {
     super(message, DomainExceptionCodes.UNAUTHORIZED);
   }
 }
