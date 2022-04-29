d61931580b5214416b39829656ccab21f6bf9fd9
Rafael Papastamatiou <rafael@papastamatiou.com>
2022-04-29 12:17:55 -0300

    feat(transactions): add transactions and assets repositories

---
+++ src/application/providers/repositories/transactions.repository.ts
@@ -0,0 +1,10 @@
+import { Transaction } from '@domain/entities/transaction';
+import { FindByIdWithAuth, FindWithAuth } from './repository.protocols';
+
+export interface TransactionsRepository {
+  find(params: FindWithAuth): Promise<Transaction[]>;
+  findById(params: FindByIdWithAuth): Promise<Transaction | undefined>;
+  add(transaction: Transaction): Promise<void>;
+  update(transaction: Transaction): Promise<void>;
+  generateId(): Promise<string>;
+}
