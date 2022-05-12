a18075d65a10d672fbb736d9a458201888457cf0
Rafael Papastamatiou <rafael@papastamatiou.com>
2022-05-12 17:13:31 -0300

    feat: create FetchStocks use case factory

---
+++ src/main/factories/useCases/stocks/fetch-stocks.factory.ts
@@ -0,0 +1,12 @@
+import { FetchStocks } from '@application/useCases/stocks/fetch-stocks';
+import { MongoStocksRepository } from '@infra/database/mongodb/repositories/mongo-stocks.repository';
+import { Redis } from '@infra/redis';
+import { TwelvedataStocks } from '@infra/twelvedata/twelvedata-stocks';
+
+export function makeFetchStocks() {
+  const cache = new Redis();
+  const stocksProvider = new TwelvedataStocks(cache);
+  const stocksRepository = new MongoStocksRepository(cache);
+
+  return new FetchStocks(stocksProvider, stocksRepository);
+}
