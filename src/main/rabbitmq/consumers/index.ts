403434c38f2857ded615f7e10d3301d6d9ef1059
Rafael Papastamatiou <rafael@papastamatiou.com>
2022-05-12 17:17:24 -0300

    feat(rabbitmq): add RabbitMQ consumers setup file

---
+++ src/main/rabbitmq/consumers/index.ts
@@ -0,0 +1,13 @@
+import { setupMongoose } from '@infra/database/mongodb/mongo-helper';
+import { setupRabbitMQ } from '@infra/rabbitmq/rabbitmq';
+import { setupRedisClient } from '@infra/redis';
+
+async function setup() {
+  await setupMongoose();
+  await setupRedisClient();
+  await setupRabbitMQ();
+
+  await import('./twelvedata-consumer');
+}
+
+setup();
