87d0550e1d7b5303404843946a0c7a2160e2a8fb
Rafael Papastamatiou <rafael@papastamatiou.com>
2022-06-05 17:49:40 -0300

    feat: save AssetType id on assets import

--- src/application/useCases/assets/fetch-assets.ts
+++ src/application/useCases/assets/fetch-assets.ts
@@ -1,4 +1,5 @@
 import { Etfs } from '@application/providers/etfs';
+import { AssetTypesRepository } from '@application/providers/repositories/asset-types.repository';
 import { AssetsRepository } from '@application/providers/repositories/assets.repository';
 import { Stocks } from '@application/providers/stocks';
 import { Asset, AssetTypes } from '@domain/entities/asset';
@@ -11,6 +12,7 @@ const acceptedStocksRegex = /(stock, reit)/i;
 export class FetchAssets implements FetchAssetsInterface {
   constructor(
     private assetsRepository: AssetsRepository,
+    private assetTypesRepository: AssetTypesRepository,
     private stocksProvider: Stocks,
     private etfsProvider: Etfs,
   ) {}
@@ -19,15 +21,20 @@ export class FetchAssets implements FetchAssetsInterface {
     // FETCH DATA
     const fetchStocksPromise = this.stocksProvider.find();
     const fetchEtfsPromise = this.etfsProvider.find();
+    const getAssetTypesIdsPromise = this.getAssetTypesIds();
 
-    const results = await Promise.all([fetchStocksPromise, fetchEtfsPromise]);
+    const results = await Promise.all([
+      fetchStocksPromise,
+      fetchEtfsPromise,
+      getAssetTypesIdsPromise,
+    ]);
 
     let [stocksArray] = results;
     stocksArray = stocksArray.filter((stockData) =>
       stockData.type.match(acceptedStocksRegex),
     );
 
-    const [, etfsArray] = results;
+    const [, etfsArray, assetTypesIds] = results;
 
     // GENERATE IDS
     const stocksIdArray: string[] = [];
@@ -51,9 +58,11 @@ export class FetchAssets implements FetchAssetsInterface {
     for (const [index, id] of stocksIdArray.entries()) {
       const stockData = stocksArray[index];
 
+      const assetType = getStockType(stockData.type);
+
       const stock = Asset.create(id, {
         ...stockData,
-        type: getStockType(stockData.type),
+        type: assetTypesIds[assetType],
         micCode: stockData.mic_code,
       });
 
@@ -68,7 +77,7 @@ export class FetchAssets implements FetchAssetsInterface {
 
       const etf = Asset.create(id, {
         ...etfData,
-        type: AssetTypes.ETF,
+        type: assetTypesIds.ETF,
         micCode: etfData.mic_code,
       });
 
@@ -82,7 +91,52 @@ export class FetchAssets implements FetchAssetsInterface {
 
     await this.assetsRepository.import(assets);
   }
+
+  private async getAssetTypesIds(): Promise<AssetTypesIds> {
+    const assetTypes = await this.assetTypesRepository.find();
+
+    const stockType = assetTypes.find((assetType) =>
+      assetType.name.match(/stock/i),
+    );
+
+    const etfType = assetTypes.find((assetType) =>
+      assetType.name.match(/etf/i),
+    );
+
+    const reitType = assetTypes.find((assetType) =>
+      assetType.name.match(/reit/i),
+    );
+
+    // const stockId = assetTypes.find((assetType) =>
+    //   assetType.name.match(/stock/i),
+    // );
+    // const stockId = assetTypes.find((assetType) =>
+    //   assetType.name.match(/stock/i),
+    // );
+
+    const types = [stockType, etfType, reitType];
+
+    for (const type of types) {
+      if (!type) {
+        throw new Error(
+          "Can't find required asset types. Seed the database with the correct values (Stock, ETF, REIT, Crypto, Index).",
+        );
+      }
+    }
+
+    return {
+      Stock: stockType.id,
+      ETF: etfType.id,
+      REIT: reitType.id,
+      Crypto: undefined,
+      Index: undefined,
+    };
+  }
 }
+
+type AssetTypesIds = {
+  [key in AssetTypes]: string;
+};
 
 const isReitRegex = /reit/i;
 
