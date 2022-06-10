fb60398fe57724fb70a2b3b587860bf7b94dd67e
Rafael Papastamatiou <rafaelpapas1412@gmail.com>
2022-06-10 18:37:07 -0300

    feat: add route

--- src/main/http/routes/wallet.routes.ts
+++ src/main/http/routes/wallet.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { adaptExpressRoute } from '../adapters/express-route.adapter';
+import { makeGetUserWalletCompositionByAssetTypeController } from '../factories/controllers/wallet/get-user-wallet-composition-by-asset-type.controller.factory';
 import { makeGetUserWalletCompositionController } from '../factories/controllers/wallet/get-user-wallet-composition.controller.factory';
 
 const walletRoutes = Router();
@@ -7,6 +8,11 @@ const walletRoutes = Router();
 walletRoutes.get(
   '/composition',
   adaptExpressRoute(makeGetUserWalletCompositionController()),
+);
+
+walletRoutes.get(
+  '/compositionByType',
+  adaptExpressRoute(makeGetUserWalletCompositionByAssetTypeController()),
 );
 
 export { walletRoutes };
